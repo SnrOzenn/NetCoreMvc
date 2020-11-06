e909d05f98eb1c63a7708255938988d527bdc834
Muhammed Orhan Tasbilek <muhammed.tasbilek@consultant.turkcell.com.tr>
2020-11-06 19:12:53 +0300

    .

---
+++ Patika/Patika.Entity/Base/EntityBase.cs
@@ -0,0 +1,30 @@
+﻿using Patika.Entity.Authentication;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Patika.Entity.Base
+{
+    public abstract class EntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("CreatedBy")]
+        public int CreatedById { get; set; }
+
+        [ForeignKey("UpdatedBy")]
+        public int? UpdatedById { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public virtual User CreatedBy { get; set; }
+        public virtual User UpdatedBy { get; set; }
+    }
+}
